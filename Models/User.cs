559302001acf765fe34a55ca8c7839a99779316a
JIDS <santosjohnisrael@gmail.com>
2022-11-15 16:31:49 +0800

    Initial Commit

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Full Name")]
+        [Required]
+        [MaxLength(48, ErrorMessage = "pls")]
+        public string Name { get; set; }
+
+        [DisplayName("Birth Date")]
+        [Required(ErrorMessage = "PLEASE")]
+
+        [DisplayFormat(DataFormatString = "{0:MMM, dd yyyy}")]
+        public DateTime Birthdate { get; set; }
+    }
+}
